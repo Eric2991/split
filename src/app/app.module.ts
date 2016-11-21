29dd1c4c805295333b72b630b7af37829aa11550
Eric Yamane <eyamane21@gmail.com>
2016-11-21 08:32:38 -0600

    Added misc. page and pipe declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { AppComponent } from './app';
+import { TabsPage } from '../pages/tabs/tabs';
 import { DashboardPage } from '../pages/dashboard/dashboard';
 import { LoginPage } from '../pages/login/login';
 import { GroupsPage } from '../pages/groups/groups';
 import { GroupViewPage } from '../pages/group-view/group-view';
 import { SessionViewPage } from '../pages/session-view/session-view';
+import { SettingsPage } from '../pages/settings/settings';
+
+import { ListMaxPipe } from './pipes/listMax.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListMaxPipe,
+    TabsPage,
     DashboardPage,
     LoginPage,
     GroupsPage,
     GroupViewPage,
-    SessionViewPage
+    SessionViewPage,
+    SettingsPage
   ],
   imports: [
     IonicModule.forRoot(AppComponent)
@@ -22,11 +29,13 @@ import { SessionViewPage } from '../pages/session-view/session-view';
   bootstrap: [IonicApp],
   entryComponents: [
     AppComponent,
+    TabsPage,
     DashboardPage,
     LoginPage,
     GroupsPage,
     GroupViewPage,
-    SessionViewPage
+    SessionViewPage,
+    SettingsPage
   ],
   providers: []
 })
---
+++ src/app/pipes/listMax.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'listMax'})
+export class ListMaxPipe implements PipeTransform {
+    transform(list: string[], limit: number): string {
+        let result: string = "", numRemaining: number = list.length - limit;
+
+        for (let i=0; i<Math.min(limit, list.length); i++) {
+            result += list[i] + ", ";
+            console.log(list[i]);
+        }
+
+        // Get rid of last comma
+        result = result.slice(0,-2);
+        
+        result = list.length < limit ? result : result + " + " + numRemaining + " more";
+        return result;
+    }
+}
