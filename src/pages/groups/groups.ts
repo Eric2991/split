d74d8f531f25cbc129bde8b13e25678d9a5c3fd5
Eric Yamane <eyamane21@gmail.com>
2016-11-22 15:33:17 -0600

    Initial Groups page

--- src/pages/groups/groups.ts
+++ src/pages/groups/groups.ts
@@ -9,8 +9,21 @@ export class GroupsPage {
 
   constructor(public navCtrl: NavController) {}
 
-  ionViewDidLoad() {
-    console.log('Hello GroupsPage Page');
-  }
+  sessions = [
+    {
+      groupName: 'Roommates',
+      groupMembers: ['Zak', 'Matthew'],
+      amountDueTotal: '$12.41',
+      amountOwedTotal: '$4.30',
+      endingDate: '12/4/2016'
+    },
+    {
+      groupName: 'Lorem Ipsum',
+      groupMembers: ['Richie', 'Steve', 'David', 'Jerry', 'Chris', 'Matthew'],
+      amountDueTotal: '$12.41',
+      amountOwedTotal: '$4.30',
+      endingDate: '12/4/2016'
+    }
+  ];
 
 }
