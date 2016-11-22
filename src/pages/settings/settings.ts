16f573d2510a8ffefb99ab5c56a546ff8d44e405
Eric Yamane <eyamane21@gmail.com>
2016-11-22 15:34:56 -0600

    Prelim settings page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { AccountPage } from '../account/account';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
+  name: string = "Eric Yamane";
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController,
+              public modalController: ModalController) {}
 
-  ionViewDidLoad() {
-    console.log('Hello SettingsPage Page');
+  presentModal(): void {
+    let modal = this.modalController.create(AccountPage);
+    modal.present();
   }
-
 }
